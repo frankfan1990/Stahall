e856bd686f7b09a709c73229abd5921400e6f999
frankfan <769683385@qq.com>
2014-12-19 14:03:31 +0800

    frankfan-12-19 || 14:03

---
+++ Stahall/Stahall/HallEvalutionIlerItemViewController.h
@@ -0,0 +1,13 @@
+//
+//  HallEvalutionIlerItemViewController.h
+//  Stahall
+//
+//  Created by frankfan on 14/12/19.
+//  Copyright (c) 2014年 Rching. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HallEvalutionIlerItemViewController : UIViewController
+
+@end
